4d6d9a5fd815c933cb58f82b23805c6aa22d96af
Hamza Khan <hz.khan@mail.utoronto.ca>
2021-01-23 15:01:02 -0500

    Added audio to the game as well as on click when interacting with objects - Hamza

---
+++ Solitude/Assets/MagicCube.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicCube : MonoBehaviour
+{
+
+    private Animator _animator;
+    private bool isClicked = false;
+    public playAudio gameAudio;
+    public AudioSource myAudioSource;
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        gameAudio = GameObject.FindObjectOfType<playAudio>();
+        myAudioSource = GetComponent<AudioSource>();
+    }
+
+    void OnMouseDown()
+    {   
+        isClicked = !(isClicked);
+
+        if (isClicked) {
+            gameAudio.togglePlay();
+            myAudioSource.Play();
+        }
+        else {
+            myAudioSource.Stop();
+            gameAudio.togglePlay();
+        }
+
+        _animator.SetBool("isClicked", isClicked);
+    }
+
+}
