4e60b61690bc4c46508e1229771e368c0eebb754
Hamza Khan <hz.khan@mail.utoronto.ca>
2021-02-12 15:08:14 -0500

    Added scenes 2 and 3 - Hamza

---
+++ Solitude/Assets/Script/Lever1Controller.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class LeverController : MonoBehaviour, ISHoldable
+{
+    public GameObject bridge;
+    public GameObject otherLever;
+    private int _holdcounter;
+    private int _holdMax;
+    private bool _holding;
+    public float bridgeSpeed = 0.1f;
+    public float bridgeDistance = 2f;
+    public bool isFix;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _holding = false;
+        _holdcounter = 0;
+        _holdMax = (int)(Math.Ceiling(bridgeDistance / bridgeSpeed));
+        isFix = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFix)
+        {
+            return;
+        }
+        if (_holding)
+        {
+            if (_holdcounter < _holdMax)
+            {
+                _holdcounter += 1;
+                bridge.transform.position += new Vector3(bridgeSpeed, 0, 0);
+                if (fullyExtend())
+                {
+                    LeverController otherLeverController = otherLever.GetComponent<LeverController>();
+                    if (otherLeverController.fullyExtend())
+                    {
+                        isFix = true;
+                        otherLeverController.isFix = true;
+                    }
+                }
+            }
+        }
+        else
+        {
+            if (_holdcounter > 0)
+            {
+                _holdcounter -= 1;
+                bridge.transform.position -= new Vector3(bridgeSpeed, 0, 0);
+            }
+        }
+        
+    }
+
+    public bool fullyExtend()
+    {
+        return _holdcounter == _holdMax;
+    }
+
+    void ISHoldable.holdStart()
+    {
+        _holding = true;
+    }
+
+    void ISHoldable.holdEnd()
+    {
+        _holding = false;
+    }
+}
---
+++ Solitude/Assets/Script/LeverController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Lever1Controller : MonoBehaviour, ISHoldable
+{
+    public GameObject bridge;
+    private int _holdcounter;
+    private int _holdMax;
+    private bool _holding;
+    public float bridgeSpeed = 0.1f;
+    public float bridgeDistance = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _holding = false;
+        _holdcounter = 0;
+        _holdMax = (int)(Math.Ceiling(bridgeDistance / bridgeSpeed));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_holding)
+        {
+            if (_holdcounter < _holdMax)
+            {
+                _holdcounter += 1;
+                bridge.transform.position += new Vector3(bridgeSpeed, 0, 0);
+            }
+        }
+        else
+        {
+            if (_holdcounter > 0)
+            {
+                _holdcounter -= 1;
+                bridge.transform.position -= new Vector3(bridgeSpeed, 0, 0);
+            }
+        }
+        
+    }
+
+    void ISHoldable.holdStart()
+    {
+        _holding = true;
+    }
+
+    void ISHoldable.holdEnd()
+    {
+        _holding = false;
+    }
+}
