92c4a3f841b17065f63c2f0d32622ba8b66032f0
Hamza Khan <hz.khan@mail.utoronto.ca>
2021-03-15 12:22:22 -0400

    More updates - Hamza

---
+++ Solitude/Assets/Script/Scene 6/Scene6LeverController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Scene6LeverController : MonoBehaviour, ISHoldable
+{
+    public GameObject bridgeLeft;
+    public GameObject bridgeRight;
+    public GameObject fallTrigger;
+    private int _holdcounter;
+    private int _holdMax;
+    private bool _holding;
+    public float bridgeSpeed = 0.1f;
+    public float bridgeDistance = 2f;
+    public bool isFix;
+    Animator animator;
+    AudioSource activeAudio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        _holding = false;
+        _holdcounter = 0; 
+        _holdMax = (int)(Math.Ceiling(bridgeDistance / bridgeSpeed));
+        isFix = false;
+        animator = GetComponent<Animator>();
+        animator.SetBool("IsDown", false);
+        activeAudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFix)
+        {
+            return;
+        }
+        if (_holding)
+        {
+            if (_holdcounter < _holdMax)
+            {
+                _holdcounter += 1;
+                bridgeLeft.transform.position += new Vector3(bridgeSpeed, 0, 0);
+                bridgeRight.transform.position -= new Vector3(bridgeSpeed, 0, 0);
+                if (fullyExtend())
+                {
+                    isFix = true;
+                    fallTrigger.SetActive(false);
+                }
+            }
+        }
+        else
+        {
+            if (_holdcounter > 0)
+            {
+                _holdcounter -= 1;
+                bridgeLeft.transform.position -= new Vector3(bridgeSpeed, 0, 0);
+                bridgeRight.transform.position += new Vector3(bridgeSpeed, 0, 0);
+            }
+        }
+        
+    }
+
+    public bool fullyExtend()
+    {
+        return _holdcounter == _holdMax;
+    }
+
+    void ISHoldable.holdStart()
+    {
+        _holding = true;
+        animator.SetBool("IsDown", true);
+        activeAudio.Play();
+    }
+
+    void ISHoldable.holdEnd()
+    {
+        _holding = false;
+        animator.SetBool("IsDown", false);
+        activeAudio.Play();
+    }
+}
